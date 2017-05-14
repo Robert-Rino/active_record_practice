a12d4a7c478e391f9b7a565d291cdc49fe502a0c
Robert-Rino <a901002666@gmail.com>
2017-05-14 14:45:18 +0800

    add grand_children relation

--- active_record_example.rb
+++ active_record_example.rb
@@ -19,4 +19,5 @@ end
 class Person < ActiveRecord::Base
   belongs_to :parent, class_name: Person
   has_many :children , class_name: Person, foreign_key: :parent_id
+  has_many :grandchildren, class_name: Person, through: :children, source: :children
 end
